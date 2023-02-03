f32466834b096d0bf1841325d0257f927fface5e
david nelband <torun.wikstrom@gmail.com>
2023-02-03 14:01:33 +0100

    update links in header

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,22 @@
-import styles from '../styles/Header.module.css';
-import Image from 'next/image';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import FeedIcon from '@mui/icons-material/Feed';
-import { Button } from '@mui/material';
+import styles from "../styles/Header.module.css";
+import Image from "next/image";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FeedIcon from "@mui/icons-material/Feed";
+import { Button } from "@mui/material";
 
 const Header = ({ about }) => {
   return (
-    <section id='header' className={styles.header}>
-      <Image src='/key.jpg' objectFit='cover' layout='fill' />
+    <section
+      id="header"
+      className={styles.header}
+    >
+      <Image
+        src="/key.jpg"
+        objectFit="cover"
+        layout="fill"
+      />
       <div className={styles.wrapper}>
         <p className={styles.b}>Frontend Developer</p>
         <p className={styles.a}>Torun Wikström</p>
@@ -34,57 +41,66 @@ const Header = ({ about }) => {
           working relationships.`}
         </p>
         <div className={styles.buttonContainer}>
-          <a href='mailto:torun.wikstrom@gmail.com'>
+          <a href="mailto:torun.wikstrom@gmail.com">
             <Button
               sx={{
-                color: 'white',
-                background: '#a33327',
-                fontFamily: 'Montserrat',
-                '&:hover': { background: '#c7493a' },
+                color: "white",
+                background: "#a33327",
+                fontFamily: "Montserrat",
+                "&:hover": { background: "#c7493a" },
               }}
-              variant='contained'
+              variant="contained"
             >
               <EmailIcon sx={{ marginRight: 1 }} /> Email
             </Button>
           </a>
-          <a href='https://www.linkedin.com/in/torun-alenius-a77011220/'>
+          <a
+            href="https://www.linkedin.com/in/torun-alenius-wikstr%C3%B6m-a77011220/"
+            target="_blank"
+          >
             <Button
               sx={{
-                color: 'white',
-                background: '#a33327',
-                fontFamily: 'Montserrat',
+                color: "white",
+                background: "#a33327",
+                fontFamily: "Montserrat",
 
-                '&:hover': { background: '#c7493a' },
+                "&:hover": { background: "#c7493a" },
               }}
-              variant='contained'
+              variant="contained"
             >
               <LinkedInIcon sx={{ marginRight: 1 }} /> LinkedIn
             </Button>
           </a>
-          <a href='https://github.com/TorunW'>
+          <a
+            href="https://github.com/TorunW"
+            target="_blank"
+          >
             <Button
               sx={{
-                color: 'white',
-                background: '#a33327',
-                fontFamily: 'Montserrat',
+                color: "white",
+                background: "#a33327",
+                fontFamily: "Montserrat",
 
-                '&:hover': { background: '#c7493a' },
+                "&:hover": { background: "#c7493a" },
               }}
-              variant='contained'
+              variant="contained"
             >
               <GitHubIcon sx={{ marginRight: 1 }} /> Github
             </Button>
           </a>
-          <a href='/resume.pdf'>
+          <a
+            href="/resume.pdf"
+            target="_blank"
+          >
             <Button
               sx={{
-                color: 'white',
-                background: '#a33327',
-                fontFamily: 'Montserrat',
+                color: "white",
+                background: "#a33327",
+                fontFamily: "Montserrat",
 
-                '&:hover': { background: '#c7493a' },
+                "&:hover": { background: "#c7493a" },
               }}
-              variant='contained'
+              variant="contained"
             >
               <FeedIcon sx={{ marginRight: 1 }} />
               Resume
